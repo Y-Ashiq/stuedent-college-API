7d6e7c0e8e95ecd680d01f1acecc26948e4615ae
Youssef Alashiq <y.alashiq11@gmail.com>
2024-04-27 20:37:31 +0300

    route section

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const fs = require("fs");
 const url = require("url");
 
 
+
 const students = JSON.parse(fs.readFileSync("./student.json", "utf-8"));
 const courses = fs.readFileSync("./courses.json", "utf-8");
 const department = fs.readFileSync("./department.json", "utf-8");
-console.log(students[0]);
+
+// const student = students.find((el) => {
+  
+  
+//   console.log(student);
+
+// });
+
 
 
 const server = http.createServer((req, res) => {
@@ -21,7 +29,19 @@ const server = http.createServer((req, res) => {
 
     res.end(JSON.stringify(students));
 
+  } else if (req.url == "/students" && req.method == "POST") {
 
+    req.on("data", chunk => {
+
+
+      students.push(JSON.parse(chunk));
+      console.log(students.includes(JSON.parse(chunk).name));
+      fs.writeFileSync("./student.json", JSON.stringify(students));
+
+
+
+    })
+    res.end("user added");
 
 
   }
